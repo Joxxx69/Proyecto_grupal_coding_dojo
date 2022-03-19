266031cdaa9d5ea46093095b86aa91142c9d7c61
JeanRamirez1996 <95257932+JeanRamirez1996@users.noreply.github.com>
2022-03-18 19:48:46 -0500

    Update plate.routes.js

--- backend/server/routes/plate.routes.js
+++ backend/server/routes/plate.routes.js
@@ -5,4 +5,5 @@ module.exports = function(app) {
     app.post('/api/createPlate', controller.createPlate );
     app.get('/api/getAll', controller.getAll);
     app.get('/api/getRandom/:ing1/:ing2/:ing3', controller.randomPlate);
+    app.patch('/api/edit_plato/:id', PlatoController.updatePlato);
 }  
