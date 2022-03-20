a283b5cec0012dd8cbe8f71fe8792d09bb19bcd3
Eavilesz <ernesto-av@hotmail.com>
2022-03-19 20:10:00 -0500

    Correccion en rutas

--- backend/server/routes/plate.routes.js
+++ backend/server/routes/plate.routes.js
@@ -4,13 +4,13 @@ module.exports = function (app) {
   app.post("/api/createPlate", controller.createPlate);
   //app.get("/api/getAll", controller.getAll);
   app.get("/api/getRandom/:ing1/:ing2/:ing3", controller.randomPlate);
-  app.get("/api/plate/:name", controller.getPlate);
-  app.put("/api/update/:id", controller.updateRecipe);
-  app.delete("/api/recipe/:id", controller.deleteRecipe);
+  app.get("/api/plate/:name", controller.getPlateByName);
+  app.put("/api/update/:id", controller.updatePlate);
+  app.delete("/api/plate/:id", controller.deletePlate);
   //ruta de prueba
   //rutas Jean
   app.get("/api/getAll", controller.getAll);
-  app.get('/api/plate/:id', controller.getPlateById);
-  app.post('/api/new_plate', controller.createPlate);
-  app.patch('/api/edit_favorite_plate/:id', controller.updateIsFavoritePlato);
+  app.get("/api/plate/:id", controller.getPlateById);
+  app.post("/api/new_plate", controller.createPlate);
+  app.patch("/api/edit_favorite_plate/:id", controller.updateIsFavoritePlato);
 };
