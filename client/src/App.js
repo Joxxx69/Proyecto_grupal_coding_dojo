061d938576284cea694b61673da8688c8c7fbd30
Joxxx69 <santiago.gualotuna@hotmail.com>
2022-03-20 17:20:29 -0500

    probar carusel

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import logo from "./logo.svg";
-import "./App.css";
-import CarouselDisplay from "./components/CarouselDisplay";
+import logo from './logo.svg';
+import './App.css';
+import CarouselDisplay from './components/CarouselDisplay'
 
 function App() {
   return (
