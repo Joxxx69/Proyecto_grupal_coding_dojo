0d514a3182db5f6cb192d650d52d876c5c699c3a
Joxxx69 <santiago.gualotuna@hotmail.com>
2022-03-20 12:30:51 -0500

    modificaciones

--- backend/server/controller/category.controller.js
+++ backend/server/controller/category.controller.js
@@ -2,29 +2,23 @@ const Category = require("../models/categoria.model");
 
 const createCategory = (req, res) => {
   const { nameCategory, photoUrl } = req.body;
-  const newCategory = new Category({
-    nameCategory,
-    photoUrl,
-  });
-  newCategory
-    .save()
-    .then((category) => res.json(category))
-    .catch((err) => console.log("there was an error", err));
+  const newCategory = new Category({nameCategory, photoUrl});
+  newCategory.save()
+  .then((category) => res.json(category))
+  .catch((err) => console.log("there was an error", err));
 };
 
-const getAllCategories = (request, response) => {
+const getAllCategories = (req, res) => {
   Category.find()
-    .then((allCategories) => response.json({ recipe: allCategories }))
+    .then((allCategories) => res.json({ recipe: allCategories }))
     .catch((err) =>
-      response.json({ message: "Something went wrong", error: err })
+      res.json({ message: "Something went wrong", error: err })
     );
 };
-const updateCategory = (request, response) => {
-  Category.findOneAndUpdate({ _id: request.params.id }, request.body, {
-    new: true,
-  })
-    .then((updateCategory) => response.json(updateCategory))
-    .catch((err) => response.status(400).json(err));
+const updateCategory = (req, res) => {
+  Category.findOneAndUpdate({ _id: req.params.id }, req.body, {new: true,})
+    .then((updateCategory) => res.json(updateCategory))
+    .catch((err) => res.status(400).json(err));
 };
 module.exports = {
   createCategory,
