881a7c07dbb97b8f364acde5fa382bbfb3148966
Eavilesz <ernesto-av@hotmail.com>
2022-03-21 14:48:33 -0500

    Se modifico componente carrusel

--- client/src/components/CarouselDisplay.js
+++ client/src/components/CarouselDisplay.js
@@ -2,9 +2,7 @@ import React, { useState, useEffect } from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import axios from "axios";
-
 const responsive = {
-
   superLargeDesktop: {
     // the naming can be any, depends on you.
     breakpoint: { max: 4000, min: 3000 },
@@ -23,35 +21,100 @@ const responsive = {
     items: 1,
   },
 };
-
 const CarouselDisplay = () => {
   const [categories, setCategories] = useState([]);
-
+  const [plates, setPlates] = useState([]);
+  const [nameCategory, setNameCategory] = useState("");
+  const [isDisplayed, setIsDisplayed] = useState(false);
   useEffect(() => {
     axios.get("http://localhost:8000/api/getAllCategories").then((res) => {
       setCategories(res.data);
-      //console.log(categories);
+      console.log(res.data);
     });
   }, []);
 
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/getAll").then((res) => {
+      setPlates(res.data);
+      console.log(res.data);
+    });
+  }, []);
+
+  // const clickHandler = () => {
+  //   setca;
+  // };
+
   return (
-    <Carousel responsive={responsive}>
-      {categories.map((val, idx) => {
-        return (
-          <div className="card" style={{ width: "18rem" }}>
-            <img
-              style={{ height: "12rem" }}
-              className="card-img-top"
-              src={val.photoUrl}
-              alt="Card image cap"
-            ></img>
-            <div className="card-body">
-              <h5 className="card-title">{val.nameCategory}</h5>
-            </div>
-          </div>
-        );
-      })}
-    </Carousel>
+    <div>
+      <Carousel responsive={responsive}>
+        {categories.map((val, idx) => {
+          return (
+            <a
+              onClick={(e) => {
+                setNameCategory(val.nameCategory);
+                setIsDisplayed(true);
+              }}
+              key={idx}
+              href="#"
+              className="card link-success"
+              style={{ width: "18rem" }}
+            >
+              <img
+                style={{ height: "12rem" }}
+                className="card-img-top"
+                src={val.photoUrl}
+                alt="Card image cap"
+              ></img>
+              <div className="card-body">
+                <h5 className="card-title">{val.nameCategory}</h5>
+              </div>
+            </a>
+          );
+        })}
+      </Carousel>
+
+      {isDisplayed && (
+        <div
+          style={{ height: "30rem" }}
+          className=" row border border-dark overflow-auto"
+        >
+          <h1>Categoria: {nameCategory} </h1>
+
+          {isDisplayed &&
+            plates
+              .filter((val) => val.category.nameCategory === nameCategory)
+              .map((val, idx) => {
+                return (
+                  <div key={idx} className="col-sm-4">
+                    <div className="card" style={{ width: "18rem" }}>
+                      <img
+                        style={{ height: "12rem" }}
+                        className="card-img-top"
+                        src={val.photo}
+                        alt="Card image cap"
+                      ></img>
+                      <div className="card-body">
+                        <h5 className="card-title">
+                          <a href="#" className="link-success">
+                            {val.nameplate}
+                          </a>
+                        </h5>
+                        <p className="card-text ">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Quisque rhoncus dui sit amet mi mollis, id
+                          pellentesque leo gravida.
+                        </p>
+                        <a href="#" className="btn btn-outline-info">
+                          Favorito
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+        </div>
+      )}
+    </div>
   );
 };
 export default CarouselDisplay;
--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -2,71 +2,110 @@
 //import axios from 'axios';
 //import {Link} from '@reach/router'
 import PlateRandom from "../components/PlateRandom";
-import CarouselDisplay from '../components/CarouselDisplay'
+import CarouselDisplay from "../components/CarouselDisplay";
 
+const Main = (props) => {
+  return (
+    <div className="container py-4">
+      <div className="d-flex ">
+        <img src={props.image_Logo} alt="logo" width="100" height="100" />
+        <h2 className="align-self-center mx-auto pe-5">Recetas Ninja</h2>
+      </div>
 
-const Main = (props)=> {
-
-    return (
-        <div className="container py-4">
-            
-            <div className="d-flex ">
-                <img src={props.image_Logo}  alt="logo" width="100" height="100"/> 
-                <h2 className="align-self-center mx-auto pe-5">Recetas Ninja</h2>
-
-            </div>
-        
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <button type="button" className="btn btn-light" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <button
+          type="button"
+          className="btn btn-light"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <button type="button" className="btn btn-light">
+                  Inicio
                 </button>
-                <div className="container-fluid">
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                        <button type="button" className="btn btn-light" >Inicio</button>
-                        </li>
-                        <li className="nav-item">
-                        <button type="button" className="btn btn-light" >Recetas</button>
-                        </li>
-                        <li className="nav-item">
-                        <button type="button" className="btn btn-light" >Nuevas Recetas</button>
-                        </li>
-                        <li className="nav-item dropdown">
-                        <button type="button" className="btn btn-light" id="navbarDropdown"  data-bs-toggle="dropdown" aria-expanded="false">
-                            Tiempo
-                        </button>
-                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><button className="dropdown-item" >10 - 20 minutos</button></li>
-                            <li><button className="dropdown-item" >20 - 30 minutos</button></li>
-                            <li><button className="dropdown-item" >30 - 40 minutos</button></li>
-                        </ul>
-                        </li>
-                        <li className="nav-item">
-                        <button type="button" className="btn btn-light" >Iniciar Sesion</button>
-                        </li>
-                    </ul>
-                    <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Buscador de recetas" aria-label="Search"/>
-                        <button className="btn btn-outline-success" type="submit">Buscar</button>
-                    </form>
-                    </div>
-                </div>
-            </nav>
-            <h2>Imagenes con Nombres de Comida</h2>
-            <CarouselDisplay/>
-            <PlateRandom/>
-            <footer>
-                <div className="social">
-                    <a href=""><i className="icon ion-social-instagram"></i></a>
-                    <a href=""><i className="icon ion-social-snapchat"></i></a>
-                    <a href=""><i className="icon ion-social-twitter"></i></a>
-                    <a href=""><i className="icon ion-social-facebook"></i></a>
-                </div>
-                <p className="copyright">Recetas Ninja © 2022</p>
-            </footer>
+              </li>
+              <li className="nav-item">
+                <button type="button" className="btn btn-light">
+                  Recetas
+                </button>
+              </li>
+              <li className="nav-item">
+                <button type="button" className="btn btn-light">
+                  Nuevas Recetas
+                </button>
+              </li>
+              <li className="nav-item dropdown">
+                <button
+                  type="button"
+                  className="btn btn-light"
+                  id="navbarDropdown"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Tiempo
+                </button>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <button className="dropdown-item">10 - 20 minutos</button>
+                  </li>
+                  <li>
+                    <button className="dropdown-item">20 - 30 minutos</button>
+                  </li>
+                  <li>
+                    <button className="dropdown-item">30 - 40 minutos</button>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <button type="button" className="btn btn-light">
+                  Iniciar Sesion
+                </button>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Buscador de recetas"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Buscar
+              </button>
+            </form>
+          </div>
         </div>
-        //Lo subo para que vean como esta pero ya le pongo un logo, las otras ventanas y un footer y si alcanzo le pongo colores
-    )
-}
+      </nav>
+      <h2>Categorias:</h2>
+      <CarouselDisplay />
+      <PlateRandom />
+      <footer>
+        <div className="social">
+          <a href="">
+            <i className="icon ion-social-instagram"></i>
+          </a>
+          <a href="">
+            <i className="icon ion-social-snapchat"></i>
+          </a>
+          <a href="">
+            <i className="icon ion-social-twitter"></i>
+          </a>
+          <a href="">
+            <i className="icon ion-social-facebook"></i>
+          </a>
+        </div>
+        <p className="copyright">Recetas Ninja © 2022</p>
+      </footer>
+    </div>
+    //Lo subo para que vean como esta pero ya le pongo un logo, las otras ventanas y un footer y si alcanzo le pongo colores
+  );
+};
 export default Main;
