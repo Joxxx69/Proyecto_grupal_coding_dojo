5f34064c346bc6f3e9e5f9a210d6ab0de7ccf21d
Joxxx69 <santiago.gualotuna@hotmail.com>
2022-03-21 21:06:56 -0500

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Main from './views/Main'
 import Logo from './images/logo_ninja.webp'
+import { Routes} from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
       <Main image_Logo={Logo}/>
+      <Routes>
+      </Routes>
     </div>
   );
 }
