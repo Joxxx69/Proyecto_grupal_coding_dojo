5287b7f2bd632ad4b439168f566cedc885767610
Eavilesz <ernesto-av@hotmail.com>
2022-03-23 18:21:08 -0500

    Se añadio formulario para crear categorias

---
+++ client/src/components/ModalForm.jsx
@@ -0,0 +1,96 @@
+import react, { useState } from "react";
+import axios from "axios";
+
+const ModalForm = (props) => {
+  const { categoryIsCreated, setCategoryIsCreated } = props;
+  const [nameCategory, setNameCategory] = useState("");
+  const [photoUrl, setPhotoUrl] = useState("");
+
+  const onClickHandler = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:8000/api/new-category", {
+      nameCategory: nameCategory,
+      photoUrl: photoUrl,
+    });
+    setCategoryIsCreated(!categoryIsCreated);
+  };
+  return (
+    <>
+      <button
+        type="button"
+        className="btn btn-outline-success"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Añadir categoría
+      </button>
+
+      <div
+        className="modal fade centered"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          {/* <form onSubmit={SubmitHandler}> */}
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Añadir categoría
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <label className="fw-bold" htmlFor="nameCategory">
+                Nombre de la categoría:
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="nameCategory"
+                aria-describedby="emailHelp"
+                onChange={(e) => setNameCategory(e.target.value)}
+              ></input>
+
+              <label className="fw-bold" htmlFor="photoUrl">
+                Url de la imágen:
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="photoUrl"
+                aria-describedby="emailHelp"
+                onChange={(e) => setPhotoUrl(e.target.value)}
+              ></input>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Cerrar
+              </button>
+              <button
+                type="submit"
+                className="btn btn-success"
+                data-bs-dismiss="modal"
+                onClick={onClickHandler}
+              >
+                Guardar
+              </button>
+            </div>
+          </div>
+          {/* </form> */}
+        </div>
+      </div>
+    </>
+  );
+};
+export default ModalForm;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,108 +1,131 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import Logo from '../images/logo_ninja.webp'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Logo from "../images/logo_ninja.webp";
 
-const Navbar = ({children}) => {
-    
-    return(
-        <div className='container py-4'>
-             <div className="d-flex ">
-                <img src={Logo} alt="logo" width="100" height="100" />
-                <h2 className="align-self-center mx-auto pe-5">Recetas Ninja</h2>
-            </div>
+const Navbar = ({ children }) => {
+  let navigate = useNavigate();
+  return (
+    <div className="container py-4">
+      <div className="d-flex ">
+        <img src={Logo} alt="logo" width="100" height="100" />
+        <h2 className="align-self-center mx-auto pe-5">Recetas Ninja</h2>
+      </div>
 
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <button
+          type="button"
+          className="btn btn-light"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
                 <button
-                    type="button"
-                    className="btn btn-light"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
+                  type="button"
+                  className="btn btn-light"
+                  onClick={(e) => navigate("/")}
                 >
-                    <span className="navbar-toggler-icon"></span>
+                  Inicio
                 </button>
-                <div className="container-fluid">
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <button type="button" className="btn btn-light">
-                                    Inicio
-                                </button>
-                            </li>
-                            <li className="nav-item">
-                                <button type="button" className="btn btn-light">
-                                    Recetas
-                                </button>
-                            </li>
-                            <li className="nav-item">
-                                <button type="button" className="btn btn-light">
-                                    Nuevas Recetas
-                                </button>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <button
-                                    type="button"
-                                    className="btn btn-light"
-                                    id="navbarDropdown"
-                                    data-bs-toggle="dropdown"
-                                    aria-expanded="false"
-                                >
-                                    Tiempo
-                                </button>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li>
-                                        <Link className="dropdown-item" to={`/recipes/times/${10}/${20}`}>10 - 20 minutes</Link>
-                                    </li>
-                                    <li>
-                                        <Link className="dropdown-item" to={`/recipes/times/${20}/${30}`}>20 - 30 minutes</Link>
-                                    </li>
-                                    <li>
-                                        <Link className="dropdown-item" to={`/recipes/times/${30}/${60}`}>30 - 60 minutes</Link>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li className="nav-item">
-                                <button type="button" className="btn btn-light">
-                                    Iniciar Sesion
-                                </button>
-                            </li>
-                        </ul>
-                        <form className="d-flex">
-                            <input
-                                className="form-control me-2"
-                                type="search"
-                                placeholder="Buscador de recetas"
-                                aria-label="Search"
-                            />
-                            <button className="btn btn-outline-success" type="submit">
-                                Buscar
-                            </button>
-                        </form>
-                    </div>
-                </div>
-            </nav>
-            {children}
-            <footer className='text-center mt-3'>
-                <div className="social">
-                    <a href="">
-                        <i className="icon ion-social-instagram"></i>
-                    </a>
-                    <a href="">
-                        <i className="icon ion-social-snapchat"></i>
-                    </a>
-                    <a href="">
-                        <i className="icon ion-social-twitter"></i>
-                    </a>
-                    <a href="">
-                        <i className="icon ion-social-facebook"></i>
-                    </a>
-                </div>
-                <p className="copyright">Recetas Ninja © 2022</p>
-            </footer>
+              </li>
+              <li className="nav-item">
+                <button type="button" className="btn btn-light">
+                  Recetas
+                </button>
+              </li>
+              <li className="nav-item">
+                <button
+                  type="button"
+                  className="btn btn-light"
+                  onClick={(e) => navigate("/add-recipe")}
+                >
+                  Nuevas Recetas
+                </button>
+              </li>
+              <li className="nav-item dropdown">
+                <button
+                  type="button"
+                  className="btn btn-light"
+                  id="navbarDropdown"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Tiempo
+                </button>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <Link
+                      className="dropdown-item"
+                      to={`/recipes/times/${10}/${20}`}
+                    >
+                      10 - 20 minutes
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="dropdown-item"
+                      to={`/recipes/times/${20}/${30}`}
+                    >
+                      20 - 30 minutes
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="dropdown-item"
+                      to={`/recipes/times/${30}/${60}`}
+                    >
+                      30 - 60 minutes
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <button type="button" className="btn btn-light">
+                  Iniciar Sesion
+                </button>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Buscador de recetas"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Buscar
+              </button>
+            </form>
+          </div>
         </div>
-    );
-}
+      </nav>
+      {children}
+      <footer className="text-center mt-3">
+        <div className="social">
+          <a href="">
+            <i className="icon ion-social-instagram"></i>
+          </a>
+          <a href="">
+            <i className="icon ion-social-snapchat"></i>
+          </a>
+          <a href="">
+            <i className="icon ion-social-twitter"></i>
+          </a>
+          <a href="">
+            <i className="icon ion-social-facebook"></i>
+          </a>
+        </div>
+        <p className="copyright">Recetas Ninja © 2022</p>
+      </footer>
+    </div>
+  );
+};
 
 export default Navbar;
