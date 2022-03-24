15a046a7f6c968c6f34cab79d5a7771c708adb41
Joxxx69 <santiago.gualotuna@hotmail.com>
2022-03-24 15:33:05 -0500

    cambios

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,23 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = 8000;
-const cors = require("cors");
+const cors = require('cors');
+const port = 8000; 
+const cookieParser = require('cookie-parser');
 
 require("./server/config/proyect.confg");
+require('dotenv').config();
 
-app.use(express.json(),express.urlencoded({ extended: true }), cors());
+
+app.use(express.json(),express.urlencoded({ extended: true }));
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
+app.use(cookieParser());
+
+
 app.set("json spaces", 2);
 
 require("./server/routes/plate.routes")(app);
 require("./server/routes/category.routes")(app);
+require("./server/routes/auth.routes")(app);
 
 app.listen(port, () => {
   console.log(`successful connection on the port ${port}`);
